dc386c4f3aaa03d80d8219efaa4dbaff05e125bd
Ecole <lehack192@gmail.com>
2024-09-14 01:36:32 +0200

    resolve theme button with danger

--- app/ui/modules/user-profile/user-account/user-account.container.tsx
+++ app/ui/modules/user-profile/user-account/user-account.container.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/app/ui/design-system/button/Button";
 export const UserAccountContainer = () => {
   return (
     <div>
-      <Button action={() => console.log("first")} variant="Danger">
+      <Button action={() => console.log("first")} variant="danger">
         Déconnemion
       </Button>
     </div>

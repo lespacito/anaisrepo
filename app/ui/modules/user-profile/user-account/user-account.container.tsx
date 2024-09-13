833fdd6a1e2329e86632965ce57ab084e3aa8dfb
Ecole <lehack192@gmail.com>
2024-09-14 01:35:15 +0200

    resolve theme button

--- app/ui/modules/user-profile/user-account/user-account.container.tsx
+++ app/ui/modules/user-profile/user-account/user-account.container.tsx
@@ -3,7 +3,9 @@ import { Button } from "@/app/ui/design-system/button/Button";
 export const UserAccountContainer = () => {
   return (
     <div>
-      <Button action={() => console.log("first")}></Button>
+      <Button action={() => console.log("first")} variant="Danger">
+        Déconnemion
+      </Button>
     </div>
   );
 };

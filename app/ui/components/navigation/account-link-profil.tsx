552e8267f828b123b3c20f0190e5093bb05312b3
Ecole <lehack192@gmail.com>
2024-09-14 22:30:46 +0200

    bugs security update

--- app/ui/components/navigation/account-link-profil.tsx
+++ app/ui/components/navigation/account-link-profil.tsx
@@ -1,16 +1,17 @@
 import Link from "next/link";
-import { Typography } from "@/app/ui/design-system/typography/Typography"
+import { Typography } from "@/app/ui/design-system/typography/Typography";
 
 export const AccountNavLinkProfil = () => {
-    return(
-        <Link href="/mon-espace" className="flex items-center gap-2">
-            <div className="">
-                <Typography variant="caption2" weight="medium" className="truncate">
-                    Lespacito
-                </Typography>
-                <Typography variant="caption4" weight="medium" className="gray">
-                    Mon compte
-                </Typography>
-            </div>
-        </Link>
+  return (
+    <Link href="/mon-espace" className="flex items-center gap-2">
+      <div className="">
+        <Typography variant="caption2" weight="medium" className="truncate">
+          Lespacito
+        </Typography>
+        <Typography variant="caption4" weight="medium" className="gray">
+          Mon compte
+        </Typography>
+      </div>
+    </Link>
+  );
 };

248b1df7ecc291c7ae67ef5808a805f13b7a78e0
Ecole <lehack192@gmail.com>
2024-09-14 16:23:20 +0200

    mon-espace updated

--- app/ui/modules/user-profile/user-account/user-account.container.tsx
+++ app/ui/modules/user-profile/user-account/user-account.container.tsx
@@ -1,4 +1,7 @@
-
 export const UserAccountContainer = () => {
-  return <div className="flex justify-center pt-20 pb-40"></div>;
+  return (
+    <div className="flex justify-center pt-20 pb-40">
+      Les infos du compte ici...
+    </div>
+  );
 };

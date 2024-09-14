7ec8a10d888fdd857eea7dc9716f2fe97ba062b1
Ecole <lehack192@gmail.com>
2024-09-14 02:49:28 +0200

    forget password logic

--- app/ui/modules/authentification/forget-password/forget-password.container.tsx
+++ app/ui/modules/authentification/forget-password/forget-password.container.tsx
@@ -3,9 +3,10 @@ import { ForgetPasswordView } from "./forget-password.view";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { ForgotFormFieldsType } from "@/app/types/Forms";
 import { useToggle } from "@/app/hooks/use-toggle";
-import { sendEmailResetPassword } from "@/app/api/authentication";
+
 import { toast } from "react-toastify";
 import { useRouter } from "next/navigation";
+import { sendEmailResetPassword } from "@/app/api/authentication";
 
 export const ForgetPasswordContainer = () => {
   const router = useRouter();
@@ -33,7 +34,7 @@ export const ForgetPasswordContainer = () => {
 
   const onSubmit: SubmitHandler<ForgotFormFieldsType> = async (formData) => {
     setIsLoading(true);
-    console.log("formData", formData);
+    handleResetPassword(formData);
   };
 
   return (

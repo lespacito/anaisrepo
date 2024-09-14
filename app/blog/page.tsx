8287b2ea0602e5ddff479e2af55bf4c0edc3b6a8
Ecole <lehack192@gmail.com>
2024-09-14 19:47:38 +0200

    test blog redirection security

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,7 +5,7 @@ import { Typography } from "../ui/design-system/typography/Typography";
 export default function Blog() {
   return (
     <>
-      <Layout>
+      <Layout sessionStatus="registered">
         <Container className="py-10 space-y-5">
           <Typography variant="lead" component="div" theme="secondary">
             Bienvenue sur le blog

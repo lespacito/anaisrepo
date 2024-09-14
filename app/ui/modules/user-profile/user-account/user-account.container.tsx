1e27563dafaa218d5faa5b79729702c5e99e47d8
Ecole <lehack192@gmail.com>
2024-09-14 16:57:11 +0200

    mon-espace content updated

--- app/ui/modules/user-profile/user-account/user-account.container.tsx
+++ app/ui/modules/user-profile/user-account/user-account.container.tsx
@@ -1,6 +1,6 @@
 export const UserAccountContainer = () => {
   return (
-    <div className="flex justify-center mb-20 pt-40">
+    <div className="flex justify-center pt-20 mb-40">
       Ici le contenu de ton utilisateur
     </div>
   );

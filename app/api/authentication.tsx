f0378ca168538214627e1ff19800762aa4bae499
Ecole <lehack192@gmail.com>
2024-09-14 02:29:14 +0200

    forget password logic

--- app/api/authentication.tsx
+++ app/api/authentication.tsx
@@ -54,7 +54,7 @@ export const firebaseLogOutUser = async () => {
 export const sendEmailResetPassword = async (email: string) => {
   try {
     const userCredential = await sendPasswordResetEmail(auth, email);
-    return { data: userCredential.user };
+    return { data: email };
   } catch (error) {
     const firebaseError = error as FirebaseError;
     return {

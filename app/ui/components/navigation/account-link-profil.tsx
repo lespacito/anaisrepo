eae53f5f1feb183ec33add1d1547cb50f4ebc434
Ecole <lehack192@gmail.com>
2024-09-14 22:38:09 +0200

    dynamic changes

--- app/ui/components/navigation/account-link-profil.tsx
+++ app/ui/components/navigation/account-link-profil.tsx
@@ -1,14 +1,19 @@
 import Link from "next/link";
 import { Typography } from "@/app/ui/design-system/typography/Typography";
+import { useAuth } from "@/app/context/AuthUserContext";
 
 export const AccountNavLinkProfil = () => {
+  const { authUser } = useAuth();
+
+  const { displayName } = authUser;
+
   return (
     <Link href="/mon-espace" className="flex items-center gap-2">
-      <div className="">
+      <div className="max-w-[160px]">
         <Typography variant="caption2" weight="medium" className="truncate">
-          Lespacito
+          {displayName ? displayName : "Bienvenue"}
         </Typography>
-        <Typography variant="caption4" weight="medium" className="gray">
+        <Typography variant="caption4" weight="medium" theme="neutral">
           Mon compte
         </Typography>
       </div>

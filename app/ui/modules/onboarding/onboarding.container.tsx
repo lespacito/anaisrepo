6a2f0418ea09786d847a175fee43c94f576a9eab
Ecole <lehack192@gmail.com>
2024-09-15 11:01:01 +0200

    init onBoarding page refactor

--- app/ui/modules/onboarding/onboarding.container.tsx
+++ app/ui/modules/onboarding/onboarding.container.tsx
@@ -1,5 +1,27 @@
 import { OnBoardingView } from "./onboarding.view";
+import { useState } from "react";
 
 export const OnBoardingContainer = () => {
+  const [currentStep, setCurrentStep] = useState<number>(1];
+
+  const stepsList = [
+    {
+      id: 1,
+      label: "Bienvenue",
+      component: {step: <div>Step Welcome</div>}
+    },
+    {
+      id: 2,
+      label: "Profile",
+      component: {step: <div>Step profile</div>}
+    },
+  ];
+
+  const getCurrentStep = () => {
+    return stepsList.find((el) => el.id === currentStep);
+  }
+
+  console.log('getCurrentStep', getCurrentStep())
+
   return <OnBoardingView />;
 };

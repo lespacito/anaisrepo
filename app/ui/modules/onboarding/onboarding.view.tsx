0c43f698dca297e5544523ac76634870f54cd892
Ecole <lehack192@gmail.com>
2024-09-15 11:24:16 +0200

    refactor route bugs center element

--- app/ui/modules/onboarding/onboarding.view.tsx
+++ app/ui/modules/onboarding/onboarding.view.tsx
@@ -1,3 +1,7 @@
 export const OnBoardingView = () => {
-  return <div>Hello tout le monde</div>;
+  return (
+    <div className="flex items-center justify-center mb-40 mt-20">
+      Hello tout le monde
+    </div>
+  );
 };
